e68a3ec654455c634f8723c6e4ed7719e023c7d4
Awalnurdianto <119844678+Awalnurdianto@users.noreply.github.com>
2022-12-05 15:10:16 +0700

    Add files via upload

---
+++ desa-digital.js
@@ -0,0 +1,2 @@
+var hasil = document.getElementById("desa-digital");
+hasil.innerHTML = "<div class='text-center text-white radius20' style='font-size: 80%;background: #ffffff30;'>&copy; By Awal Nurdianto |Karangnangka Binangun | <a style='color: white' href='https://wa.me/+6282327172044'>Awal-HomePage</a>   </div>";
